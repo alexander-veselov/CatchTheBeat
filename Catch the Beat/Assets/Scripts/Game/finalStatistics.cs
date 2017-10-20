03252ec55cbb709e99d4d552cedc16b166863f47
Alexander <32273263+paniker228@users.noreply.github.com>
2017-10-20 23:41:48 +0300

    MERGE

--- Catch the Beat/Assets/Scripts/Game/finalStatistics.cs
+++ Catch the Beat/Assets/Scripts/Game/finalStatistics.cs
@@ -4,37 +4,23 @@ using UnityEngine;
 
 public class finalStatistics : MonoBehaviour {
 
-	public static int missed_fruits;
+	public static int missed_fruits=0;
 
-	public static int big_fruits_counter;
-	public static int small_fruits_counter;
-	public static int medium_fruits_counter;
+	public static int big_fruits_counter=0;
+	public static int small_fruits_counter=0;
+	public static int medium_fruits_counter = 0;
 
-	public static int comboCounter;
-	public static int maxCombo;
+	public static int comboCounter = 0;
+	public static int maxCombo = 114 ;
 
-	public static long finalScore;
+	public static long finalScore = 0;
 
-	public static float accuracy;
+	public static float accuracy = 0f;
 
 	// Use this for initialization
 	void Start () {
 		
-		missed_fruits = 0;
 
-		big_fruits_counter = 0;
-
-		small_fruits_counter = 0;
-
-		medium_fruits_counter = 0;
-
-		comboCounter = 0;
-
-		maxCombo = 0;
-
-		finalScore = 0;
-
-        accuracy = 0.0f;
 
 	}
 	
@@ -46,11 +32,35 @@ public class finalStatistics : MonoBehaviour {
 			maxCombo = comboCounter;
 		}
 
+		getAccuracy ();
+	}
+
+	void getAccuracy() {
+
 		if (big_fruits_counter > 0 || missed_fruits > 0) {
 			int sum1 = small_fruits_counter + big_fruits_counter + medium_fruits_counter;
 			int sum2 = small_fruits_counter + big_fruits_counter + medium_fruits_counter + missed_fruits;
 			accuracy = (float)sum1 / (float)sum2;
 			accuracy *= 100.0f;
 		}
+
+	}
+
+	public static void ZeroStats() {
+
+		missed_fruits = 0;
+
+		big_fruits_counter = 0;
+		small_fruits_counter = 0;
+		medium_fruits_counter = 0;
+
+		comboCounter = 0;
+		maxCombo = 0 ;
+
+		finalScore = 0;
+
+		accuracy = 0f;
+
+
 	}
 }
